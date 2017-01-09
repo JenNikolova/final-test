ab84d6b1248c02ad90e3e60be973b94eeb06854f
Jen Shen <jen.nikolova@gmail.com>
2017-01-09 21:42:56 +0200

    final-test
    My solutions

--- task2/script.js
+++ task2/script.js
@@ -3,16 +3,30 @@
 // задача 1
 
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function (a, b) {
+	return a - b
+});
 
 // задача 2
 
 var text = "The quick brown fox jumps over the lazy dog";
+var list = text.split(" ");
+
+function words(list) {
+	return list.length >= 4;
+}
 
 // задача 3
 
 
-// задача 4
-
+// задача 4 отпада
 
 // задача 5
 
+var sessionKey = getsessionKey("samplename");
+
+if (sessionKey == null) {
+	alert("cookie does not exist");
+} else {
+	alert("cookie exists");
+}
---
+++ task3/script.js
@@ -0,0 +1,4 @@
+$(function () {
+    var kidsss = JSON.parse($('kids.json').html());
+    alert(kidsss.name && kidsss.age && kidsss.color && kidsss.game kidsss.food);
+});
